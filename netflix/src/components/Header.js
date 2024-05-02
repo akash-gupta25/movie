3193c5375dc9ae2a55f1ce40a97a3528ca218b45
akash-gupta25 <107639829+akash-gupta25@users.noreply.github.com>
2024-05-01 22:34:30 -0700

    Update Header.js

--- netflix/src/components/Header.js
+++ netflix/src/components/Header.js
@@ -1,27 +1,22 @@
 import React from 'react'
 import { IoIosArrowDropdown } from "react-icons/io";
-import { useSelector, useDispatch } from "react-redux" 
+import {useSelector,useDispatch} from "react-redux" 
 import { API_END_POINT } from '../utils/constant';
 import axios from "axios";
 import { setUser } from '../redux/userSlice';
-import { useNavigate } from "react-router-dom";
+import {useNavigate} from "react-router-dom";
 import toast from "react-hot-toast";
 import { setToggle } from '../redux/movieSlice';
 
 const Header = () => { 
-    const user = useSelector((store) => store.app.user);
-    const toggle = useSelector(store => store.movie.toggle);
+    const user = useSelector((store)=>store.app.user);
+    const toggle = useSelector(store=>store.movie.toggle);
     const dispatch = useDispatch();
     const navigate = useNavigate();
 
     const logoutHandler = async () => {
         try {
-            const res = await axios.get(`${API_END_POINT}/logout`, {
-                headers: {
-                    'Access-Control-Allow-Origin': '*'
-                    // You can also add other CORS headers if needed
-                }
-            });
+            const res = await axios.get(`${API_END_POINT}/logout`);
             if(res.data.success){
                 toast.success(res.data.message);
             }
