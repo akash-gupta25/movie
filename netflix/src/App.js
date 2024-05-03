ef0eefc9977e939d0c7050e663cf2b928d6e518d
akash-gupta25 <107639829+akash-gupta25@users.noreply.github.com>
2024-05-02 23:41:12 -0700

    Update App.js

--- netflix/src/App.js
+++ netflix/src/App.js
@@ -1,19 +1,16 @@
-import React from 'react';
 import Body from "./components/Body";
-import { Toaster } from 'react-hot-toast';
+import { Toaster } from 'react-hot-toast'; 
 import MovieDialog from "./components/MovieDialog";
-import { BrowserRouter as Router } from 'react-router-dom'; // Change createBrowserRouter to BrowserRouter
-
+ 
 function App() {
-    return (
-        <Router> {/* Wrap the app with Router */}
-            <div>
-                <Body />
-                <Toaster />
-                <MovieDialog />
-            </div>
-        </Router>
-    );
+  return (
+    <div>
+       <Body/>
+       <Toaster/>  
+       <MovieDialog/>
+    </div>
+  );
 }
 
 export default App;
+
