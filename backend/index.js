d0a81fff676253b5baec2141c36f3c670f3f22f6
akash-gupta25 <107639829+akash-gupta25@users.noreply.github.com>
2024-05-08 10:26:48 -0700

    Update index.js

--- backend/index.js
+++ backend/index.js
@@ -25,7 +25,7 @@ const corsOptions = {
 app.use(cors(corsOptions));
  
 // api
-app.use("/api/v1/user", userRoute);
+app.use("", userRoute);
 
 app.listen(process.env.PORT,() => {
     console.log(`Server listen at port ${process.env.PORT}`);
